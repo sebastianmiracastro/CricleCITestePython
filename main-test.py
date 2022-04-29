4407be4ccd647ef0394faf6c93bacb92cf44465b
sebastianmiracastro <smira44@misena.edu.co>
2022-04-28 23:28:43 -0500

    bug?

--- main-test.py
+++ main-test.py
@@ -2,6 +2,7 @@ from main import Basic
 
 def TestBasic():
     assert Basic(2, 3) == 5
+    assert Basic(5, 5) == 11
     print("El aplicativo pasa la integracion, pasa a la entrega o despliegue.")
 
 if __name__ == '__main__':
