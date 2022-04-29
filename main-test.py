13effd6c998f25136391b8976fdaa875e05c9c54
sebastianmiracastro <smira44@misena.edu.co>
2022-04-29 00:56:14 -0500

    try to error

--- main-test.py
+++ main-test.py
@@ -2,7 +2,8 @@ from main import Basic
 
 def TestBasic():
     assert Basic(2, 3) == 5
-    assert Basic(5, 5) == 11
+    assert Basic(5, 5) == 10
+    assert Basic(6, 6) == 13
     print("El aplicativo pasa la integracion, pasa a la entrega o despliegue.")
 
 if __name__ == '__main__':
