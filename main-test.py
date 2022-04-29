c2403dbc6264b8cad521f9c7eae673ec1c57e86b
sebastianmiracastro <smira44@misena.edu.co>
2022-04-29 00:51:07 -0500

    Example To New Push

--- main-test.py
+++ main-test.py
@@ -1,7 +1,7 @@
 from main import Basic
 
 def TestBasic():
-    assert Basic(2, 3) == 5
+    assert Basic(3, 3) == 6
     print("El aplicativo pasa la integracion, pasa a la entrega o despliegue.")
 
 if __name__ == '__main__':
